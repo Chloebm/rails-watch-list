a51fce65ff4f94af8cbe501ea4d4c6a9fa7426c6
Chloe Bodart <chloebodartmiro@gmail.com>
2021-05-28 12:32:01 +0100

    App working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -10,22 +15,25 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-
     if @list.save
-      redirect_to @list, notice: 'List was successfully created.'
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
-  def show
-    @list = List.find(params[:id])
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
-private
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
   end
-
 end
